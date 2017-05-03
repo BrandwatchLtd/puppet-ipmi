77ec4f5a51e63f06d8b3ff5342ec1288df17fdfc
Luke Alexander <luke@brandwatch.com>
2017-05-03 13:52:31 +0100

    as per SYSDESK-1720, trying to add some security

---
+++ manifests/auth.pp
@@ -0,0 +1,17 @@
+# == Defined resource type: ipmi::auth
+# title should be one of: Callback, User, Operator, Admin
+# the preferred auth type available atm is MD5, marginally better than MD2 or plain
+
+define ipmi::auth (
+  $auth_type = 'MD5',
+){
+  require ::ipmi
+
+  validate_re($title, '^Callback$|^User$|^Operator$|^Admin$', 'Auth title should be one of: Callback|User|Operator|Admin')
+  validate_re($auth_type, '^NONE$|^MD2$|^MD5$|^PASSWORD$', 'Auth type should be one of: NONE|MD2|MD5|PASSWORD')
+
+  exec { "ipmi_set_auth_${title}":
+    command => "/usr/bin/ipmitool lan set 1 auth ${title} ${auth_type}",
+    onlyif  => "/usr/bin/test \"$(ipmitool lan print | grep -A 4 'Auth Type Enable' | grep \"${title}\" sed -e 's/.* : //g')\" != \"${auth_type}\"",
+  }
+}
