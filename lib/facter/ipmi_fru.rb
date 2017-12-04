a1c3cd32fa2a06ab263b99d8ecb35bde906de148
Luke Alexander <luke@brandwatch.com>
2017-12-04 10:00:23 +0000

    adding ipmi product name field

--- lib/facter/ipmi_fru.rb
+++ lib/facter/ipmi_fru.rb
@@ -15,6 +15,8 @@ def parse_ipmitool_output ipmitool_output
       add_ipmi_fact("fru_board_product", $1)
     when /^Product Serial\s*:\s+(\S.*)/
       add_ipmi_fact("fru_product_serial", $1)
+    when /^Product Name\s*:\s+(\S.*)/
+      add_ipmi_fact("fru_product_name", $1)
     end
   end
 end
